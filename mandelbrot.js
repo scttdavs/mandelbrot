7d4af60da979a13aefc599f523c605324d17cc96
Scott Davis <sc.ttdav.s@gmail.com>
2017-05-03 13:23:25 -0400

    more refactor

--- mandelbrot.js
+++ mandelbrot.js
@@ -12,25 +12,19 @@
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  const width = 2 * (canvas.width / canvas.height); // -2 to 2
+  const width = 1.6 * (canvas.width / canvas.height); // -2 to 2
 
   const ctx = canvas.getContext('2d');
   const imgData = ctx.createImageData(canvas.width, 1);
 
   const x_interval = width * 2 / canvas.width; // -2 to 2
-  const y_interval = 4 / canvas.height; // -2 to 2
+  const y_interval = 3.2 / canvas.height; // -2 to 2
 
   const m = {
+    maxIterations: 50,
+    escapeRadius: 5,
     ctx,
     imgData,
-
-    square(value) {
-      return Math.pow(value, 2);
-    },
-
-    absValue(complexNumber) {
-      return Math.sqrt(m.square(complexNumber.real) + m.square(complexNumber.imaginary));
-    },
 
     complexNumber(real, imaginary) {
       return {
@@ -39,59 +33,48 @@
       };
     },
 
-    getAlpha(iterations) {
-      return iterations / 80 * 255;
+    getColor(iterations) {
+      // if (iterations[0] === 80) return 255;
+      return 255 - (iterations[0] / m.maxIterations * 255);
     },
 
-    // z^2 + c
-    // (zR^2 - zI^2, 2 * zR * zI) + (cR, cI)
-    getZ(z, c) {
-      // console.time("z");
-      const ret = m.complexNumber(m.square(z.real) - m.square(z.imaginary) + c.real, 2 * z.real * z.imaginary + c.imaginary);
-      // console.timeEnd("z");
-      return ret;
-    },
-
-    getIterations(c, escapeRadius, maxIterations) {
+    getIterations(c) {
       let Zr = 0;
       let Zi = 0;
       let Tr = 0;
       let Ti = 0;
       let n  = 0;
 
-      for ( ; n < maxIterations && (Tr + Ti) <= escapeRadius; ++n) {
+      for ( ; n < m.maxIterations && (Tr + Ti) <= m.escapeRadius; ++n) {
         Zi = 2 * Zr * Zi + c.imaginary;
         Zr = Tr - Ti + c.real;
         Tr = Zr * Zr;
         Ti = Zi * Zi;
       }
 
-      return n;
+      return [n, Ti + Tr];
     },
 
     draw() {
       let yPixel = 0
-      for (let y = 2; y >= -2; y -= y_interval) {
+      for (let y = 1.6; y >= -1.6; y -= y_interval) {
         let offset = 0;
         yPixel++;
 
         // build line of pixel data to render
-        // console.time("oneline");
         for(let x = -width; x <= width; x += x_interval) {
-          // console.time("iterations");
-          const iterations = m.getIterations(m.complexNumber(x, y), 10, 80);
-          // console.timeEnd("iterations");
+          const iterations = m.getIterations(m.complexNumber(x, y));
 
-          m.imgData.data[offset++] = 0;
-          m.imgData.data[offset++] = 0;
-          m.imgData.data[offset++] = 0;
-          m.imgData.data[offset++] = m.getAlpha(iterations);
+          const color = m.getColor(iterations);
+
+          m.imgData.data[offset++] = color;
+          m.imgData.data[offset++] = color;
+          m.imgData.data[offset++] = color;
+          m.imgData.data[offset++] = 255;
         }
-        // console.timeEnd("oneline");
+
         // render it
-        // setTimeout(() => ctx.putImageData(imgData, 0, y), 0);
         m.ctx.putImageData(imgData, 0, yPixel);
-        // console.log(imgData);
       }
     }
   };
