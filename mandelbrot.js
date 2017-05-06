a888320740421e49a5b46693ec98fad5280551a1
Scott Davis <sc.ttdav.s@gmail.com>
2017-05-06 16:41:44 -0400

    change to state object

--- mandelbrot.js
+++ mandelbrot.js
@@ -25,9 +25,6 @@
   const ctx = canvas.getContext('2d');
   const imgData = ctx.createImageData(canvas.width, 1);
 
-  const x_interval = width * 2 / canvas.width; // -2 to 2
-  const y_interval = yMax * 2 / canvas.height; // -2 to 2
-
   const helpers = {
     getEl(id) {
       return document.getElementById(id);
@@ -52,28 +49,44 @@
       if (typeof value === "string") return value == "true";
       return value;
     }
+  };
+
+  const paramMap = {
+    maxIterations: "mi",
+    escapeRadius: "er",
+    color: "co",
+    julia: "j",
+    c: "c",
+    left: "l",
+    top: "t",
+    right: "r",
+    bottom: "b"
   }
 
-
   const m = {
-    maxIterations: parseInt(helpers.getValue("mi", helpers.getEl("maxIterations").value, 50)),
     escapeRadius: parseInt(helpers.getValue("er", helpers.getEl("escapeRadius").value, 5)),
-    y_interval,
-    x_interval,
-    boundaries: {
+    y_interval() {
+      return Math.abs(m.state.top - m.state.bottom) / canvasOverlay.height;
+    },
+    x_interval() {
+      return Math.abs(m.state.left - m.state.right) / canvasOverlay.width;
+    },
+    state: {
+      maxIterations: parseInt(helpers.getValue("mi", helpers.getEl("maxIterations").value, 50)),
+      escapeRadius: parseInt(helpers.getValue("er", helpers.getEl("escapeRadius").value, 5)),
+      color: helpers.getBoolean(helpers.getValue("co", helpers.getEl("color").checked, false)),
+      julia: helpers.getBoolean(helpers.getValue("j", helpers.getEl("julia").checked, false)),
+      c: [-0.123, 0.745], // TODO update this
+      left: parseFloat(helpers.getValue("l", -width)),
       top: parseFloat(helpers.getValue("t", yMax)),
       right: parseFloat(helpers.getValue("r", width)),
-      bottom: parseFloat(helpers.getValue("b", -yMax)),
-      left: parseFloat(helpers.getValue("l", -width))
+      bottom: parseFloat(helpers.getValue("b", -yMax))
     },
     ctx,
     overlayCtx,
     imgData,
     yPixel: 0,
     y: parseFloat(helpers.getValue("t", yMax)),
-    c: [-0.123, 0.745], // TODO update this
-    color: helpers.getBoolean(helpers.getValue("co", helpers.getEl("color").checked, false)),
-    julia: helpers.getBoolean(helpers.getValue("j", helpers.getEl("julia").checked, false)),
     lastUpdatedAt: 0,
     totalTime: document.getElementById("totalTime"),
 
@@ -104,32 +117,32 @@
       const n = iterations.number - Math.log2(Math.log2(iterations.absoluteValue));
 
       // grayscale
-      if (!m.color) {
-        const value = 255 - (n / m.maxIterations * 255)
+      if (!m.state.color) {
+        const value = 255 - (n / m.state.maxIterations * 255)
         return [value, value, value];
       }
 
       // color
       if (n <= 1) return [255, 255, 255];
-      if (n >= m.maxIterations - 1) return [0, 0, 0];
+      if (n >= m.state.maxIterations - 1) return [0, 0, 0];
 
-      const value = n / m.maxIterations;
+      const value = n / m.state.maxIterations;
 
       // adjusting hue and value to make colors look better (blue only)
       return m.hsvToRgb(.5 + value / 2, 1, 1 - value);
     },
 
     getIterations(real, imaginary) {
-      let Zr = m.julia ? real : 0,
-          Zi = m.julia ? imaginary : 0,
+      let Zr = m.state.julia ? real : 0,
+          Zi = m.state.julia ? imaginary : 0,
           tempR,
           tempI,
           n  = 0,
           abs = 0,
-          max = m.maxIterations,
-          escape = m.escapeRadius,
-          Cr = m.julia ? m.c[0] : real,
-          Ci = m.julia ? m.c[1] : imaginary;
+          max = m.state.maxIterations,
+          escape = m.state.escapeRadius,
+          Cr = m.state.julia ? m.state.c[0] : real,
+          Ci = m.state.julia ? m.state.c[1] : imaginary;
 
       for ( ; n < max && abs <= escape; n++) {
         tempR = Math.pow(Zr, 2) - Math.pow(Zi, 2) + Cr;
@@ -157,8 +170,9 @@
 
     drawSingleLine(x) {
       let offset = 0;
+      const x_interval = m.x_interval();
       // build line of pixel data to render
-      for(; x <= m.boundaries.right; x += m.x_interval) {
+      for(; x <= m.state.right; x += x_interval) {
         const iterations = m.getIterations(x, m.y);
         const color = m.getColor(iterations);
 
@@ -173,9 +187,10 @@
       if (!m.startTime) m.startTime = Date.now();
       m.yPixel++;
 
-      m.drawSingleLine(m.boundaries.left);
+      m.drawSingleLine(m.state.left);
       m.ctx.putImageData(imgData, 0, m.yPixel);
-      m.y -= m.y_interval
+      const y_interval = m.y_interval();
+      m.y -= y_interval;
 
       if (m.yPixel <= window.innerHeight) {
         // not done, keep drawing
@@ -190,7 +205,7 @@
         }
       } else {
         // done drawing, reset
-        m.y = m.boundaries.top;
+        m.y = m.state.top;
         m.lastUpdatedAt = 0;
         m.yPixel = 0;
         m.updateProgressLine();
@@ -204,36 +219,20 @@
       return [parseFloat(values[0]), parseFloat(values[1])];
     },
 
-    pushState(replace) {
-      const params = [
-        `mi=${m.maxIterations}`,
-        `er=${m.escapeRadius}`,
-        `co=${m.color}`,
-        `j=${m.julia}`,
-        `c=${m.c}`,
-        `l=${m.boundaries.left}`,
-        `t=${m.boundaries.top}`,
-        `r=${m.boundaries.right}`,
-        `b=${m.boundaries.bottom}`
-      ];
-      // TODO add getState method, maybe with a toParams method too
-      // change keys to not be param names?
-      const state = {
-        mi: m.maxIterations,
-        er: m.escapeRadius,
-        co: m.color,
-        j: m.julia,
-        c: m.c,
-        l: m.boundaries.left,
-        t: m.boundaries.top,
-        r: m.boundaries.right,
-        b: m.boundaries.bottom
-      };
+    getParams() {
+      const params = [];
+      for (let key in m.state) {
+        params.push(`${paramMap[key]}=${m.state[key]}`);
+      }
 
+      return `?${params.join("&")}`
+    },
+
+    pushState(replace) {
       if (replace) {
-        history.replaceState(state, "Mandelbrot",  `?${params.join("&")}`);
+        history.replaceState(m.state, "Mandelbrot",  m.getParams());
       } else {
-        history.pushState(state, "Mandelbrot", `?${params.join("&")}`);
+        history.pushState(m.state, "Mandelbrot", m.getParams());
       }
     },
 
@@ -242,7 +241,7 @@
     },
 
     setState(id, value) {
-      m[id] = value;
+      m.state[id] = value;
       const el = helpers.getEl(id);
       if (typeof value === "boolean") {
         el.checked = value;
@@ -286,31 +285,40 @@
 
       canvasOverlay.addEventListener("mouseup", (e) => {
         // TODO this needs state change logic
-        m.boundaries = {
-          left: m.boundaries.left + m.x_interval * zoomBox[0],
-          right: m.boundaries.left + m.x_interval * zoomBox[2],
-          top: m.boundaries.top - m.y_interval * zoomBox[1],
-          bottom: m.boundaries.top - m.y_interval * zoomBox[3]
-        };
-        m.y = m.boundaries.top;
-        m.x_interval = Math.abs(m.boundaries.left - m.boundaries.right) / canvasOverlay.width;
-        m.y_interval = Math.abs(m.boundaries.top - m.boundaries.bottom) / canvasOverlay.height;
+        // order is significant here
+        const x_interval = m.x_interval();
+        const y_interval = m.y_interval();
+
+        m.state.right = m.state.left + x_interval * zoomBox[2];
+        m.state.left = m.state.left + x_interval * zoomBox[0];
+        m.state.bottom = m.state.top - y_interval * zoomBox[3];
+        m.state.top = m.state.top - y_interval * zoomBox[1];
+
+        m.y = m.state.top;
 
         zoomBox = null;
         m.overlayCtx.clearRect(0, 0, canvasOverlay.width, canvasOverlay.height);
         m.draw();
+        m.pushState();
       });
     },
 
     updateState(obj = {}) {
       // TODO use setState with dirty logic so it only draws once
-      helpers.getEl("color").checked = m.color = obj.co;
-      helpers.getEl("julia").checked = m.julia = obj.j;
-      helpers.getEl("maxIterations").value = m.maxIterations = obj.mi;
-      helpers.getEl("escapeRadius").value = m.escapeRadius = obj.er;
+      helpers.getEl("color").checked = m.state.color = obj.color;
+      helpers.getEl("julia").checked = m.state.julia = obj.julia;
+      helpers.getEl("maxIterations").value = m.state.maxIterations = obj.maxIterations;
+      helpers.getEl("escapeRadius").value = m.state.escapeRadius = obj.escapeRadius;
+
+      // TODO make function for this?
+      m.state.left = obj.left;
+      m.state.top = obj.top;
+      m.state.right = obj.right;
+      m.state.bottom = obj.bottom;
+      m.y = m.state.top;
 
       helpers.getEl("c").value = obj.c; // need to format this first
-      m.c = obj.c;
+      m.state.c = obj.c;
 
       m.draw();
     },
