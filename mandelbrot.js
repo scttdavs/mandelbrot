767f51214d2c7d061681bf0b3aef1b9d93a29629
Scott Davis <sc.ttdav.s@gmail.com>
2018-03-05 22:33:26 -0500

    fix save image

--- mandelbrot.js
+++ mandelbrot.js
@@ -312,7 +312,14 @@
 
       helpers.getEl("reset").addEventListener("click", m.reset);
 
-      helpers.getEl("download").addEventListener("click", () => window.open(canvas.toDataURL("image/png"), "_blank"));
+      helpers.getEl("download").addEventListener("click", () => {
+        const string = canvas.toDataURL("image/png");
+        const iframe = `<iframe src='${string}' frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe></iframe>`;
+        const win = window.open("", "_blank");
+        win.document.open();
+        win.document.write(iframe);
+        win.document.close();
+      });
 
       // INPUT CONTROLS
       helpers.getEl("maxIterations").addEventListener("change", (e) => m.setState("maxIterations", parseInt(e.target.value)));
